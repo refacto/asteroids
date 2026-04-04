a6d4f0d78a7608cff9e0591cfedf50f817a3b79b
Reto Brunner <reto@slightlybroken.com>
2026-04-04 13:28:40 +0200

    player: implement firing of bullets
    For now this puts bullets behind a timeout so that they don't stick
    around forever.
    
    This commit implements it such that the bullets are fired independently
    of the current ship speed.
    I've tried to implement it by synchronizing the initial velocity
    to the ship, but that makes the bullets curve.

---
+++ shot.c
@@ -0,0 +1,59 @@
+#include "shot.h"
+#include "objects.h"
+#include <raylib.h>
+#include <raymath.h>
+
+constexpr int SHOT_RADIUS = 2;
+constexpr float STARTING_LIFETIME = 4.5;
+constexpr float SHOT_SPEED = 5;
+
+void shot_init(struct Shot *shot) {
+	*shot = (struct Shot){
+		.object =
+			{
+				.color = BLACK,
+				.max_velocity = SHOT_SPEED,
+			},
+		.lifetime = STARTING_LIFETIME,
+	};
+}
+void shot_update(struct Shot *shot) {
+	if (!shot->active) {
+		return;
+	}
+	shot->lifetime -= GetFrameTime();
+	if (shot->lifetime < 0) {
+		shot->active = false;
+		return;
+	}
+	// FIXME: this should be passed in to update
+	shot_move(shot, (Vector2){.x = 800, .y = 450});
+}
+
+void shot_draw(struct Shot *shot) {
+	if (!shot->active) {
+		return;
+	}
+	DrawCircle((int)shot->object.position.x, (int)shot->object.position.y,
+			   SHOT_RADIUS, shot->object.color);
+}
+
+void shot_move(struct Shot *shot, Vector2 screen_dimensions) {
+	object_move(&shot->object);
+	object_wrap_screen(&shot->object, screen_dimensions, SHOT_RADIUS * 2);
+}
+
+void shot_fire(struct Shot *shot, Vector2 position, float rotation) {
+	shot->object.position = position;
+	shot->object.rotation = rotation;
+	shot->object.velocity = (Vector2){
+		.x = (float)(SHOT_SPEED * sin((float)rotation * DEG2RAD)),
+		.y = (float)(-SHOT_SPEED * cos((float)rotation * DEG2RAD)),
+	};
+	shot->active = true;
+	shot->lifetime = STARTING_LIFETIME;
+}
+
+void shot_set_active(struct Shot *shot, bool active) {
+	shot->active = active;
+}
--- shot.h
+++ shot.h
@@ -4,4 +4,11 @@
 struct Shot {
 	struct Object object;
 	bool active;
+	float lifetime;
 };
+
+void shot_init(struct Shot *shot);
+void shot_fire(struct Shot *shot, Vector2 position, float rotation);
+void shot_update(struct Shot *shot);
+void shot_draw(struct Shot *shot);
+void shot_move(struct Shot *shot, Vector2 screen_dimensions);
