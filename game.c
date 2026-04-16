0974be97fd42ebce236b1a2a60a95b06c1f7464f
Reto Brunner <reto@slightlybroken.com>
2026-04-16 21:36:05 +0200

    move shots to game
    Turns out that letting the player own the shots is suboptimal.
    The game needs to know what shot hit an asteroid, in order
    to tell how to split it. Game also needs to be able to drive the
    scoring and other mechanics.
    
    So move the shots as well as the input handling to struct Game.
    This needs additional player APIs to get/set the necessary state

--- game.c
+++ game.c
@@ -1,6 +1,8 @@
 #include "game.h"
 #include "asteroid.h"
+#include "input.h"
 #include "player.h"
+#include "shot.h"
 #include <raylib.h>
 #include <raymath.h>
 #include <stdlib.h>
@@ -32,6 +34,9 @@ void game_init(struct Game *game, int screenWidth, int screenHeight) {
 		last = asteroid;
 	}
 	game->asteroids = last;
+	for (int i = 0; i < MAX_NUM_SHOTS; i++) {
+		shot_init(&game->shots[i]);
+	}
 }
 
 void game_destroy(struct Game *game) {
@@ -44,36 +49,115 @@ void game_destroy(struct Game *game) {
 	}
 }
 
-void game_update(struct Game *game) {
+static struct Shot *get_inactive_shot(struct Game *game) {
+	for (int i = 0; i < MAX_NUM_SHOTS; i++) {
+		struct Shot *cur = &game->shots[i];
+		if (!cur->active) {
+			return cur;
+		}
+	}
+	return nullptr;
+}
+
+static void try_fire_shot(struct Game *game) {
+	struct Shot *shot = get_inactive_shot(game);
+	if (!shot) {
+		return; // exceeded the maximum amount of bullets
+	}
+	Vector2 startpos = player_cannon_position(&game->player);
+	shot_fire(shot, startpos, game->player.object.rotation);
+}
+
+static void update_shots(struct Game *game) {
+	for (int i = 0; i < MAX_NUM_SHOTS; i++) {
+		shot_update(&game->shots[i]);
+	}
+}
+
+static void update_asteroids(struct Game *game) {
 	struct Asteroid *asteroid = game->asteroids;
 	while (asteroid) {
 		asteroid_update(asteroid, game->screen_dimensions);
 		asteroid = asteroid->next;
 	}
-	player_update(&game->player);
-	asteroid = game->asteroids;
-	bool player_hit = false;
-	while (asteroid) {
-		enum CollisionResult res =
-			player_check_collision(&game->player, asteroid);
-		switch (res) {
-			case DESTROYED: {
-				// TODO: handle scoring
-				asteroid_stop_moving(asteroid);
-				break;
-			}
-			case PLAYER_DAMAGE: {
-				// TODO: handle life tracking
-				player_hit = true;
-				break;
-			}
-			case NO_HIT: {
-				break;
-			}
-		}
-		asteroid = asteroid->next;
+}
+
+static void handle_input(struct Game *game) {
+	constexpr float rotation_increment = 5;
+	if (input_key_down(ACTION_LEFT)) {
+		player_rotate(&game->player, -rotation_increment);
 	}
+	if (input_key_down(ACTION_RIGHT)) {
+		player_rotate(&game->player, rotation_increment);
+	}
+	constexpr float thrust_inc = 0.2f;
+	if (input_key_down(ACTION_UP)) {
+		player_thrust_inc(&game->player, thrust_inc);
+	} else {
+		player_thrust_inc(&game->player, -thrust_inc / 2);
+	}
+	if (input_key_down(ACTION_DOWN)) {
+		player_thrust_inc(&game->player, -thrust_inc);
+	}
+	if (input_key_once(ACTION_EXECUTE)) {
+		try_fire_shot(game);
+	}
+}
+
+static struct Shot *find_shot_collided(struct Game *game,
+									   struct Asteroid *asteroid) {
+	for (int i = 0; i < MAX_NUM_SHOTS; i++) {
+		struct Shot *shot = &game->shots[i];
+		if (!shot->active) {
+			continue;
+		}
+		if (shot_collide_asteroid(shot, asteroid)) {
+			return shot;
+		};
+	}
+	return nullptr;
+}
+
+static bool collide_asteroid_shots(struct Game *game,
+								   struct Asteroid *asteroid) {
+	struct Shot *shot = find_shot_collided(game, asteroid);
+	if (!shot) {
+		return false;
+	}
+	// TODO: handle scoring
+	shot_stop_moving(shot);
+	asteroid_stop_moving(asteroid);
+	return true;
+}
+
+static void handle_collisions(struct Game *game) {
+	bool player_hit = false;
+	for (struct Asteroid *asteroid = game->asteroids; asteroid;
+		 asteroid = asteroid->next) {
+		if (collide_asteroid_shots(game, asteroid)) {
+			// don't want to check for player collision
+			continue;
+		}
+		// this short circuits, so the player can only be hit once currently
+		player_hit =
+			player_hit || player_check_collision(&game->player, asteroid);
+	}
+	// TODO: handle life tracking
 	player_mark_shot(&game->player, player_hit);
+}
+
+void game_update(struct Game *game) {
+	update_asteroids(game);
+	player_update(&game->player);
+	update_shots(game);
+	handle_collisions(game);
+	handle_input(game);
+}
+
+static void draw_shots(struct Game *game) {
+	for (int i = 0; i < MAX_NUM_SHOTS; i++) {
+		shot_draw(&game->shots[i]);
+	}
 }
 
 void game_draw(struct Game *game) {
@@ -83,6 +167,7 @@ void game_draw(struct Game *game) {
 		asteroid_draw(cur);
 		cur = cur->next;
 	}
+	draw_shots(game);
 	player_draw(&game->player);
 }
 
